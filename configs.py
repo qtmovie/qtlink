a60f0e209d08a061f1af704d29ec95f448e9260c
qtmovie <150934706+qtmovie@users.noreply.github.com>
2023-11-30 21:48:47 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -13,7 +13,7 @@ class Config(object):
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "6624919731"))
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://qtmovie99:7gMCva3UFeEgHlP2@cluster0.qve685d.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1002135831842")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
