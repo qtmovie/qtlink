520a743d981e8d5942a0f0ba78c6e61493694463
qtmovie <150934706+qtmovie@users.noreply.github.com>
2024-02-01 19:58:29 +0530

    Update check_user_status.py

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -21,6 +21,6 @@ async def handle_user_status(bot, cmd):
         ).days > ban_status["ban_duration"]:
             await db.remove_ban(chat_id)
         else:
-            await cmd.reply_text("You R Banned!.. Contact @paidby99 😝", quote=True)
+            await cmd.reply_text("You R Banned!.. Contact @VJ_Botz😝", quote=True)
             return
     await cmd.continue_propagation()
